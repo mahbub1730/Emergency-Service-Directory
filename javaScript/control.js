b81c87e6360abc74976f8c9f9903adf55cade5e0
mahbub1730 <smmahbubulalam16@gmail.com>
2025-09-03 22:25:28 +0600

    add call button javascript code

--- javaScript/control.js
+++ javaScript/control.js
@@ -1,33 +1,252 @@
-  // এলিমেন্টগুলো নাও
-  const callBtn1 = document.getElementById("callBtn1");
-  const callHistory = document.getElementById("callHistory");
-  const clearBtn = document.querySelector(".col-span-1 button"); // Clear button ধরলাম
+document.getElementById("heart-1").addEventListener("click", function() {
+  // console.log("heart clicked")
+  const heartCount = parseInt(document.getElementById("heartCount").innerText)
 
-  // Call Button Click Event
-  callBtn1.addEventListener("click", function () {
-    alert("Calling Emergency Number...");
+  const clicked = heartCount + 1
+  document.getElementById("heartCount").innerText = clicked
 
-    // serviceName এবং serviceNumber নাও
-    const serviceName = document.getElementById("serviceName").innerText;
-    const serviceNumber = document.getElementById("serviceNumber").innerText;
+})
 
-    // সময় বের করো
-    const now = new Date();
-    const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+// copy Button control
+document.getElementById("copybtn").addEventListener("click", function() {
+  const copyCountNumber =parseInt(document.getElementById("copyCount").innerText)
 
-    // History item বানাও
-    const div = document.createElement("div");
-    div.classList.add("border-b", "pb-2", "mb-2");
-    div.innerHTML = `
-      <p class="font-medium">${serviceName}</p>
-      <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
-    `;
+  const totalCopy = copyCountNumber + 1
+  document.getElementById("copyCount").innerText = totalCopy
 
-    // উপরে add করো
-    callHistory.prepend(div);
-  });
+})
 
-  // Clear Button Click Event
-  clearBtn.addEventListener("click", () => {
-    callHistory.innerHTML = "";
-  });
+
+// call Button ---> 1 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 2 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 3 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 4 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 5 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 6 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 7 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 8 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// call Button ---> 9 
+document.getElementById("callBtn1").addEventListener("click", function() {
+  alert("Calling Emergency Number...")
+
+  const serviceName = document.getElementById("serviceName").innerText
+  const serviceNumber = document.getElementById("serviceNumber").innerText
+  const callHistory = document.getElementById("callHistory")
+
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+   const div = document.createElement("div");
+  div.classList.add("border-b", "pb-2", "mb-2");
+  div.innerHTML = `
+    <p class="font-medium">${serviceName}</p>
+    <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p>
+  `;
+
+  callHistory.prepend(div);
+});
+
+// Clear Button Click Event
+
+document.getElementById("clearBtn").addEventListener("click", () => {
+  callHistory.innerHTML = "";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Call Button Click Event 
+// callBtn1.addEventListener("click", function () { 
+//   alert("Calling Emergency Number..."); 
+//   // serviceName and serviceNumber copy 
+//   const serviceName = document.getElementById("serviceName").innerText; 
+//   const serviceNumber = document.getElementById("serviceNumber").innerText;
+//    // time manage 
+//    const now = new Date(); const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+//     // History item 
+//     const div = document.createElement("div"); 
+//     div.classList.add("border-b", "pb-2", "mb-2"); 
+//     div.innerHTML = `<p class="font-medium">${serviceName}</p> 
+//     <p class="text-gray-500 text-xs">${serviceNumber} • ${time}</p> ; 
+//     callHistory.prepend(div)`; });
+    
+//      // Clear Button Click Event 
+//      clearBtn.addEventListener("click", () => {
+//        callHistory.innerHTML = ""; 
+//       })
+
+
+
+
